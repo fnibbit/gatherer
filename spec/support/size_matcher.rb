0958aba26b24c18b87c83e90eb3f1558a4937485
Rachel M. Kadel <rachelkg@gmail.com>
2020-06-06 22:38:37 -0400

    import some stuff from the book codebase

--- spec/support/size_matcher.rb
+++ spec/support/size_matcher.rb
@@ -14,4 +14,8 @@ RSpec::Matchers.define :be_of_size do |expected|
   failure_message_when_negated do |actual|
     "expected project #{actual.name} not to have size #{expected}, but it did"
   end
+
+  chain :for_incomplete_tasks_only do
+    @incomplete = true
+  end
 end
